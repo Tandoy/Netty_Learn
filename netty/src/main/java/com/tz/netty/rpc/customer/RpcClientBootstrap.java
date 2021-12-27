ace0f0c62ca10eb0df27bdaf7c00923f873dfc7e
Tandoy <tangzhi1318@163.com>
2021-12-27 17:26:42 +0800

    Netty_Learn：自实现rpc

---
+++ netty/src/main/java/com/tz/netty/rpc/customer/RpcClientBootstrap.java
@@ -0,0 +1,25 @@
+package com.tz.netty.rpc.customer;
+
+import com.tz.netty.rpc.netty.RpcNettyClient;
+import com.tz.netty.rpc.publicinterface.RpcHelloService;
+
+public class RpcClientBootstrap {
+    //这里定义协议头
+    public static final String providerName = "HelloService#hello#";
+
+    public static void main(String[] args) throws Exception {
+
+        //创建一个消费者
+        RpcNettyClient customer = new RpcNettyClient();
+
+        //创建代理对象
+        RpcHelloService service = (RpcHelloService) customer.getBean(RpcHelloService.class, providerName);
+
+        for (; ; ) {
+            Thread.sleep(2 * 1000);
+            //通过代理对象调用服务提供者的方法(服务)
+            String res = service.hello("你好 dubbo~");
+            System.out.println("调用的结果 res= " + res);
+        }
+    }
+}
---
+++ netty/src/main/java/com/tz/netty/rpc/provider/RpcServerBootstrap.java
@@ -0,0 +1,10 @@
+package com.tz.netty.rpc.provider;
+
+import com.tz.netty.rpc.provider.RpcNettyServer;
+
+public class RpcServerBootstrap {
+    public static void main(String[] args) {
+        //代码代填..
+        RpcNettyServer.startServer("127.0.0.1", 7000);
+    }
+}
